9a53ad698cf902dace0671fdaecb62af5fb04355
Vanaja <vanaja.ucet@gmail.com>
2014-07-14 11:09:27 +0530

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -1,14 +1,17 @@
 #setting working directory
-setwd("D:\\DataScience\\ExploratoryDataAnalysis");
+#setwd("D:\\DataScience\\ExploratoryDataAnalysis");
+setwd("F:\\Learning\\Data Science\\data\\GitProject")
 #Read the whole data into a variable from the downloaded file 
 content <- readLines("household_power_consumption.txt")
 #Create a new file named feb.txt with the header text
-cat("Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;Sub_metering_3", file="feb.txt", append=TRUE)
+cat("Date;Time;Global_active_power;Global_reactive_power;Voltage;Global_intensity;Sub_metering_1;Sub_metering_2;Sub_metering_3", file="feb1.txt", append=TRUE)
 #Since we are going to use only data of two days extract them using grep command into the created file
-cat(grep(pattern="^1/2/2007", content, value=TRUE), file="feb.txt", append=TRUE)
-cat(grep(pattern="^2/2/2007", content, value=TRUE), file="feb.txt", append=TRUE)
+cat("\n", file="feb1.txt", append=TRUE)
+cat(grep(pattern="^1/2/2007", content, value=TRUE), file="feb1.txt", append=TRUE,sep="\n")
+cat("\n", file="feb1.txt", append=TRUE)
+cat(grep(pattern="^2/2/2007", content, value=TRUE), file="feb1.txt", append=TRUE,sep="\n")
 #Read the created file's data into a variable
-data <- read.table("feb.txt", sep = ";", header = T, na.strings = "?");
+data <- read.table("feb1.txt", sep = ";", header = T, na.strings = "?");
 #Create a column named Datetime
 data$Datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
 
@@ -34,5 +37,3 @@ legend("topright", lty = 1, col = c("black", "red", "blue"), legend = c("Sub_met
 # plot 4 (SE)
 plot(data$Datetime, data$Global_reactive_power, type = "l", xlab = "datetime", 
     ylab = "Global_reactive_power", ylim = c(0, 0.5), scale_y_continuous(breaks = c(0, 500)))
-
-
